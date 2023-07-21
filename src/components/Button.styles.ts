d9f09fd38f815e8b02af3885a2db5aecd2d2ea9f
William Roger <williamroger.frontend@gmail.com>
2023-07-21 15:20:35 -0300

    adicionado estilos globais a aplicação

--- src/components/Button.styles.ts
+++ src/components/Button.styles.ts
@@ -6,13 +6,6 @@ interface ButtonContainerProps {
   variant: ButtonVariant;
 }
 
-const buttonVariants = {
-  primary: 'purple',
-  secondary: 'orange',
-  danger: 'red',
-  success: 'green',
-}
-
 export const ButtonContainer = styled.button<ButtonContainerProps>`
   width: 100px;
   height: 40px;
