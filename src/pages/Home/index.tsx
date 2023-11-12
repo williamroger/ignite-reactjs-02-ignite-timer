ad5da8495823f41f639cd7a78ea7884df4b2cf02
William Roger <williamroger.frontend@gmail.com>
2023-11-12 11:55:54 -0300

    reduzindo o countdown

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Play } from "phosphor-react";
 import { useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as zod from 'zod';
+import { differenceInSeconds } from 'date-fns';
 
 import { 
   CountdownContainer, 
@@ -30,6 +31,7 @@ interface Cycle {
   id: string;
   task: string;
   minutesAmount: number;
+  startDate: Date;
 }
 
 export function Home() {
@@ -45,6 +47,18 @@ export function Home() {
     }
   }); 
 
+  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => { 
+        setAmountSecondsPassed(
+          differenceInSeconds(new Date(), activeCycle.startDate)
+        );
+      }, 1000);
+    }
+  }, [activeCycle]);
+  
   function handleCreateNewCycle(data: NewCycleFormData) {
     const id = String(new Date().getTime());
     const { task, minutesAmount } = data;
@@ -52,7 +66,8 @@ export function Home() {
     const newCycle: Cycle = {
       id,
       task,
-      minutesAmount
+      minutesAmount,
+      startDate: new Date(),
     }
 
     setCycles((state) => [...state, newCycle]);
@@ -60,8 +75,6 @@ export function Home() {
 
     reset();
   }
-
-  const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
