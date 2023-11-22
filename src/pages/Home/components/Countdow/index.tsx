65b8ae40dddab53187b6780307af568fdef2d2ea
William Roger <williamroger.frontend@gmail.com>
2023-11-22 01:09:48 -0300

    convertendo newcycleform para contexto

--- src/pages/Home/components/Countdow/index.tsx
+++ src/pages/Home/components/Countdow/index.tsx
@@ -1,13 +1,17 @@
-import { useContext, useEffect, useState } from "react";
+import { useContext, useEffect } from "react";
 import { differenceInSeconds } from 'date-fns';
 
 import { CountdownContainer, Separator } from "./styles";
 import { CyclesContext } from "../..";
 
 export function Countdown() {
-  const { activeCycle, activeCycleId, markCurrentCycleAsFinished } = useContext(CyclesContext);
-
-  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
+  const { 
+    activeCycle, 
+    activeCycleId, 
+    amountSecondsPassed, 
+    markCurrentCycleAsFinished,
+    setSecondsPassed, 
+  } = useContext(CyclesContext);
 
   const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
 
@@ -23,9 +27,9 @@ export function Countdown() {
 
         if (secondsDifference >= totalSeconds) {
           markCurrentCycleAsFinished();
-          setAmountSecondsPassed(totalSeconds);
+          setSecondsPassed(totalSeconds);
         } else {
-          setAmountSecondsPassed(secondsDifference);
+          setSecondsPassed(secondsDifference);
         }
       }, 1000);
 
@@ -37,7 +41,13 @@ export function Countdown() {
     return () => {
       clearInterval(interval);
     }
-  }, [activeCycle, totalSeconds, activeCycleId, markCurrentCycleAsFinished]);
+  }, [
+    activeCycle, 
+    totalSeconds, 
+    activeCycleId, 
+    markCurrentCycleAsFinished, 
+    setSecondsPassed
+  ]);
   
   const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
 
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,6 +1,6 @@
 import { createContext, useState } from "react";
 import { HandPalm, Play } from "phosphor-react";
-import { useForm } from 'react-hook-form';
+import { FormProvider, useForm } from 'react-hook-form';
 import { zodResolver } from '@hookform/resolvers/zod';
 import * as zod from 'zod';
 
@@ -37,7 +37,9 @@ interface Cycle {
 interface CyclesContextType {
   activeCycle: Cycle | undefined,
   activeCycleId: string | null,
+  amountSecondsPassed: number,
   markCurrentCycleAsFinished: () => void,
+  setSecondsPassed: (seconds: number) => void,
 }
 
 export const CyclesContext = createContext({} as CyclesContextType);
@@ -45,8 +47,9 @@ export const CyclesContext = createContext({} as CyclesContextType);
 export function Home() {
   const [cycles, setCycles] = useState<Cycle[]>([]);
   const [activeCycleId, setActiveCycleId] = useState<string | null>(null);
+  const [amountSecondsPassed, setAmountSecondsPassed] = useState(0);
 
-  const { register, handleSubmit, watch, reset } = useForm<NewCycleFormData>({
+  const newCycleForm = useForm<NewCycleFormData>({
     resolver: zodResolver(newCycleFormValidationSchema),
     defaultValues: {
       task: '',
@@ -54,7 +57,13 @@ export function Home() {
     }
   });
 
+  const { handleSubmit, watch, reset } = newCycleForm;
+
   const activeCycle = cycles.find((cycle) => cycle.id === activeCycleId);
+
+  function setSecondsPassed(seconds: number) {
+    setAmountSecondsPassed(seconds);
+  }
 
   function markCurrentCycleAsFinished() {
     setCycles(state => (
@@ -105,8 +114,18 @@ export function Home() {
   return (
     <HomeContainer>
       <form onSubmit={handleSubmit(handleCreateNewCycle)}>
-        <CyclesContext.Provider value={{ activeCycle, activeCycleId, markCurrentCycleAsFinished }}>
-          {/* <NewCycleForm /> */}
+        <CyclesContext.Provider 
+          value={{ 
+          activeCycle, 
+          activeCycleId, 
+          amountSecondsPassed,
+          markCurrentCycleAsFinished,
+          setSecondsPassed,
+          }}
+        >
+          <FormProvider {...newCycleForm}>
+            <NewCycleForm />
+          </FormProvider>
           <Countdown />
         </CyclesContext.Provider>
 
